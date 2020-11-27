3f4e073ee1eb7362d95e91125b665681dea817c1
NoDontDoIt <54070204+InvalidName662@users.noreply.github.com>
2020-11-27 22:56:14 +0100

    It works!

--- src/sammlung/guellencore/GuellenCore.java
+++ src/sammlung/guellencore/GuellenCore.java
@@ -1,6 +1,8 @@
 package sammlung.guellencore;
 
-public class GuellenCore {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GuellenCore extends JavaPlugin {
 
 	public static void main(String[] args) {
 		
